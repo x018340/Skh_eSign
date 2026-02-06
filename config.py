b08ea7bae99264489897dbcc640aac800d2f9044
x018340 <x018340@gmail.com>
2026-02-06 13:53:03 +0800

    Update config.py

--- config.py
+++ config.py
@@ -4,4 +4,4 @@ DEPLOYMENT_URL = "skhesign-jnff8wr9fkhrp6jqpsvfsjh.streamlit.app"
 SHEET_NAME = "esign"
 FONT_CH = "font_CH.ttf"
 FONT_EN = "font_EN.ttf"
-ADMIN_KEY = st.secrets["general"]["admin_password"] # Maps to your secret
+ADMIN_KEY = st.secrets["general"]["admin_password"] # Set as "1234" in your secrets
