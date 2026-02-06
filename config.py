78256c2e0329504da19cf287c4550a1861273081
x018340 <x018340@gmail.com>
2026-02-06 11:25:18 +0800

    Update config.py

--- config.py
+++ config.py
@@ -1,11 +1,14 @@
 import streamlit as st
 
-# Secrets
-GCP_CREDS = st.secrets["gcp_service_account"]
-ADMIN_PWD = st.secrets["general"]["admin_password"]
-DRIVE_ID  = st.secrets["general"]["drive_folder_id"]
-
-# Settings
+# Deployment Settings
+DEPLOYMENT_URL = "skhesign-jnff8wr9fkhrp6jqpsvfsjh.streamlit.app"
 SHEET_NAME = "esign"
-DEPLOY_URL = "skhesign-jnff8wr9fkhrp6jqpsvfsjh.streamlit.app"
-FONTS = {"CH": "font_CH.ttf", "EN": "font_EN.ttf"}
+
+# Secrets Mapping
+GCP_CREDS = st.secrets["gcp_service_account"]
+ADMIN_PASSWORD = st.secrets["general"]["admin_password"]
+DRIVE_FOLDER_ID = st.secrets["general"]["drive_folder_id"]
+
+# Assets
+FONT_CH = "font_CH.ttf"
+FONT_EN = "font_EN.ttf"
