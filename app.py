defd45305ef881fab75386b8e7d67486960bf6c5
x018340 <x018340@gmail.com>
2026-02-06 13:41:15 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,28 +1,23 @@
 import streamlit as st
-from config import ADMIN_PASSWORD
-from core.state import init_state, ensure_data_loaded
+from config import ADMIN_KEY
+from core.state import init_data, ensure_data_loaded
 from components.signin_view import show_signin
 from components.admin_view import show_admin
 
 st.set_page_config(page_title="SKH E-Sign System", page_icon="✍️", layout="wide")
 
-# 1. Initialize
-init_state()
+init_data()
 
-# 2. Check Routing
 query_params = st.query_params
-mid_param = query_params.get("mid")
-admin_key_param = query_params.get("admin_access")
+mid_param = query_params.get("mid", None)
+admin_access_param = query_params.get("admin_access", None)
 
-# 3. Logic
 if mid_param:
     ensure_data_loaded()
     show_signin(mid_param)
-
-elif admin_key_param == ADMIN_PASSWORD or st.session_state.is_admin:
+elif (admin_access_param == ADMIN_KEY) or st.session_state.is_admin:
     st.session_state.is_admin = True
     ensure_data_loaded()
     show_admin()
-
 else:
     st.error("⛔ Access Denied.")
